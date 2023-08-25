af163f9f8e9ce8176f75a2fe099e17207d2f20da
zettacamp-triyo-krismantoro <140870777+zettacamp-triyo-krismantoro@users.noreply.github.com>
2023-08-25 23:18:03 +0700

    [TASK] JS day 1
    first commit

---
+++ js_day1_task1_triyo.js
@@ -0,0 +1,15 @@
+let book1 = "Pulang by Tere Liye";
+const book2 = ["You do you by Fellexandro Ruby"];
+const book3 = "You do you by Fellexandro Ruby";
+
+book1 = "Fisika Modern";
+
+// value of const book2 can be changed
+book2[0] = "Hello Habit by fumio sasaki";
+
+// value of const book3 can't be changed
+// book3 = "Hello Habit by fumio sasaki";
+
+console.log(book1);
+console.log(String(book2));
+console.log(book3);
---
+++ js_day1_task2_triyo.js
@@ -0,0 +1,12 @@
+let book1 = "Pulang by Tere Liye";
+const book2 = ["You do you by Fellexandro Ruby"];
+
+book1 = "Fisika Modern"
+book2[0] = "Hello Habit by fumio sasaki"
+
+let mergedBook = book1 + " " + book2[0];
+
+// other option
+// let mergedBook = book1.concat(" ", book2[0]);
+
+console.log(mergedBook);
